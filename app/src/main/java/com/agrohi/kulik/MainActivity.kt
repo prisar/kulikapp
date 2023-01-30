5782038331639f91dfeb0cdc38114fdaebb87533
prisar <prisar230@gmail.com>
2023-01-30 11:13:40 +0530

    image

--- app/src/main/java/com/agrohi/kulik/MainActivity.kt
+++ app/src/main/java/com/agrohi/kulik/MainActivity.kt
@@ -3,13 +3,17 @@ package com.agrohi.kulik
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.agrohi.kulik.ui.theme.KulikTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Home()
                 }
             }
         }
@@ -30,14 +34,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Home() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween) {
+        Text("Kulik app for raiganj")
+
+        Image(
+            painter = painterResource(id = R.drawable.kulik),
+            contentDescription = "train",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        )
+
+        Text("Wiki for outsiders")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     KulikTheme {
-        Greeting("Android")
+        Home()
     }
 }
