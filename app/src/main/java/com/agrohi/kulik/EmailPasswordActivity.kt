14ffd45e7c0ca32d2e4fb50d2892571f8afa8c6e
prisar <prisar230@gmail.com>
2023-05-20 02:51:49 +0530

    call sigin in fun

--- app/src/main/java/com/agrohi/kulik/EmailPasswordActivity.kt
+++ app/src/main/java/com/agrohi/kulik/EmailPasswordActivity.kt
@@ -4,6 +4,40 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Card
+import androidx.compose.material.TextField
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.agrohi.kulik.ui.theme.KulikTheme
+import com.agrohi.kulik.ui.theme.LightBlueBg
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
@@ -15,6 +49,18 @@ class EmailPasswordActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = Firebase.auth
+
+        setContent {
+            KulikTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    EmailPasswordSignIn()
+                }
+            }
+        }
     }
 
     public override fun onStart() {
@@ -90,5 +136,75 @@ class EmailPasswordActivity : ComponentActivity() {
 
     companion object {
         private const val TAG = "EmailPassword"
+    }
+
+
+    @Composable
+    fun EmailPasswordSignIn() {
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.background(LightBlueBg)
+        ) {
+
+            Column(modifier = Modifier.background(Color.White).height(500.dp)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .height(100.dp)
+                        .padding(5.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text("Email")
+                    TextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        label = { Text("Enter email") },
+                    )
+
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .height(100.dp)
+                        .padding(5.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text("Password")
+                    TextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text("Enter password") },
+                        visualTransformation = PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+                    )
+                }
+
+                Row(
+                    verticalAlignment = Alignment.Bottom,
+                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .height(100.dp)
+                        .padding(10.dp)
+                        .background(Color.LightGray)
+                        .fillMaxWidth()
+                        .clickable() {
+                            signIn(email, password)
+                        }
+                ) {
+                    Text(
+                        "submit",
+                        style = TextStyle(fontSize = 32.sp),
+                        modifier = Modifier.padding(10.dp)
+                    )
+                }
+            }
+        }
     }
 }
