517d42b1e0b556c25b1eb8d2f2fe115715a3206c
prisar <prisar230@gmail.com>
2023-05-22 16:14:16 +0530

    create post entry to db

--- app/src/main/java/com/agrohi/kulik/AddPostActivity.kt
+++ app/src/main/java/com/agrohi/kulik/AddPostActivity.kt
@@ -1,35 +1,53 @@
 package com.agrohi.kulik
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Card
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TextField
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.agrohi.kulik.ui.theme.KulikTheme
 import com.agrohi.kulik.ui.theme.LightBlueBg
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class AddPostActivity : ComponentActivity() {
     private lateinit var auth: FirebaseAuth
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = Firebase.auth
@@ -54,26 +72,103 @@ class AddPostActivity : ComponentActivity() {
 //            userData = currentUser
         }
     }
-}
 
-@Composable
-fun AddPostScreen() {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier.background(LightBlueBg)
-    ) {
+    @Composable
+    fun AddPostScreen() {
+        var message by remember { mutableStateOf("") }
+        val db = FirebaseFirestore.getInstance()
 
-        Card(
-            shape = RoundedCornerShape(10.dp),
-            elevation = 1.dp,
-            backgroundColor = Color.White,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(all = 16.dp)
-                .height(250.dp)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.background(LightBlueBg)
         ) {
-            Text("Add post")
+
+            Card(
+                shape = RoundedCornerShape(10.dp),
+                elevation = 1.dp,
+                backgroundColor = Color.White,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(all = 16.dp)
+                    .height(250.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.background(LightBlueBg)
+                ) {
+                    Row() {
+                        Text("Add post")
+                    }
+
+                    Row() {
+                        OutlinedTextField(
+                            modifier = Modifier
+                                .fillMaxWidth().height(120.dp)
+                                .padding(start = 15.dp, top = 10.dp, end = 15.dp)
+                                .background(Color.White, RoundedCornerShape(5.dp)),
+                            shape = RoundedCornerShape(5.dp),
+                            value = message,
+                            onValueChange = { message = it },
+                            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                            maxLines = 3,
+                            label = { Text("Write the description of your post") },
+                        )
+                    }
+
+                    Row(horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.Bottom,
+                        modifier = Modifier
+                        .padding(10.dp)
+                        .height(70.dp)
+                        .fillMaxWidth()
+                        .clip(shape = RoundedCornerShape(30.dp))
+                        .background(Color.White)
+                        .clickable() {
+                            val currentUser = auth.getCurrentUser()
+                            val createdAt = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZZZZZ").format( Date())
+                            if (currentUser != null) {
+                                val data = hashMapOf(
+                                    "avatar" to currentUser.photoUrl,
+                                    "createdAt" to createdAt,
+                                    "displayName" to currentUser.displayName,
+                                    "likes" to 0,
+                                    "message" to message,
+                                    "shares" to 0,
+                                    "type" to "photo",
+                                    "userId" to currentUser.uid,
+                                    "views" to 0,
+                                )
+
+                                db
+                                    .collection("posts")
+                                    .add(data)
+                                    .addOnSuccessListener { documentReference ->
+                                        Log.d(TAG, "DocumentSnapshot written with ID: ${documentReference.id}")
+                                    }
+                                    .addOnFailureListener { e ->
+                                        Log.w(TAG, "Error adding document", e)
+                                    }
+                            }
+                        }
+                    ) {
+                        Text("Create",
+                            style = TextStyle(
+                                fontSize = 32.sp,
+                                fontWeight = FontWeight.Bold,
+                                textAlign = TextAlign.Center,
+                            ),
+                            modifier = Modifier
+                                .padding(10.dp)
+                        )
+                    }
+                }
+            }
         }
+    }
+
+    companion object {
+        private const val TAG = "AddPostActivity"
     }
 }
