6878718bab715fcc2444001036020b2e09a9633c
prisar <prisar230@gmail.com>
2024-12-26 21:59:05 +0530

    feat: add preview in homescreen

--- app/src/main/java/com/agrohi/kulik/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/agrohi/kulik/ui/screens/HomeScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -155,4 +156,11 @@ fun HomeScreen(
             }
         }
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen(navController = NavController(LocalContext.current))
 }
